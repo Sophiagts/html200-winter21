0a0a89f24c70664792f136dc12d1d0e300a61225
Sophia <sophiats@uw.edu>
2021-02-03 21:11:09 -0800

    lesson 3 assignments

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,15 @@
+let arr = ['sherlock', 'watson', 'mary', 'irene', 'moriarty']
+
+console.log (arr);
+
+arr.push('holmes');
+
+console.log (arr);
+
+arr.splice(3, 1);
+
+console.log(arr);
+
+let str = arr.join (',');
+
+console.log(str)
